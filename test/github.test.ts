4835ac959b8b66d442c4b0f1aebc688b64665a9e
Shariq Torres <enshenpa@gmail.com>
2022-02-21 05:54:16 -0800

    made sure token was optional
    I want to make more use of the GH token to pull down private repos, but that is still in the future. For right now, I'm going to take off the devKey being required.

--- test/github.test.ts
+++ test/github.test.ts
@@ -6,11 +6,12 @@ describe('github module functions', () => {
     it('should return a Github Client class when getClient is called', () => {
         let result = Client.github.getClient('examplekey')
         expect(result instanceof GithubClient).toBe(true)
+        result = Client.github.getClient()
+        expect(result instanceof GithubClient).toBe(true)
     }) 
 })
 
 describe('GithubClient class', () => {
-    
     describe('downloadRepo method',() => {
         afterEach(()=>{
             fsExtra.emptyDirSync(__dirname + "/gitreposrc")
